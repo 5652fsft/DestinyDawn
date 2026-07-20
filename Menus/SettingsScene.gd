3666329248e252c8add8b6b839b9b75ab5a6c9a5
5652fsft <1093296300@qq.com>
2026-07-20 13:19:39 +0800

    fix: move signal connections from tscn to gd _ready() (tscn inline syntax unsupported)

--- Menus/SettingsScene.gd
+++ Menus/SettingsScene.gd
@@ -4,17 +4,21 @@ const FONT = preload("res://Assets/Fonts/SourceHanSerifCN-Heavy-4.otf")
 
 @onready var name_edit = $VBoxContainer/NameEdit
 @onready var ip_edit = $VBoxContainer/IPEdit
+@onready var save_btn = $VBoxContainer/SaveButton
+@onready var back_btn = $VBoxContainer/BackButton
 
 func _ready():
 	GlobalGameData.load_defaults_if_empty()
 	name_edit.text = GlobalGameData.player_name
 	ip_edit.text = GlobalGameData.server_ip
-	for btn in [$VBoxContainer/SaveButton, $VBoxContainer/BackButton]:
+	for btn in [save_btn, back_btn]:
 		ButtonTheme.apply_menu(btn)
 		ButtonTheme.set_font(btn, 20)
 	for le in [name_edit, ip_edit]:
 		le.add_theme_font_override("font", FONT)
 		le.add_theme_font_size_override("font_size", 18)
+	save_btn.pressed.connect(_on_save_pressed)
+	back_btn.pressed.connect(_on_back_pressed)
 
 func _on_save_pressed():
 	GlobalGameData.player_name = name_edit.text
