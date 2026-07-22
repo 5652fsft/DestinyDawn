eb906470c5934466a00885085d3bd316b7aa9566
5652fsft <1093296300@qq.com>
2026-07-23 00:44:40 +0800

    fix: block camera zoom when hovered control has MOUSE_FILTER_STOP ancestor

--- UI/ScrollAcceptRichTextLabel.gd
+++
@@ -1,6 +0,0 @@
-extends RichTextLabel
-
-func _gui_input(event):
-	super._gui_input(event)
-	if event is InputEventMouseButton and (event.button_index == MOUSE_BUTTON_WHEEL_DOWN or event.button_index == MOUSE_BUTTON_WHEEL_UP):
-		accept_event()
